8b60e01e1f0e6206085090ae857ecfaea5544061
Ahmed Sheikh <ahmedos099912@gmail.com>
2026-04-21 14:33:19 +0400

    org the files

---
+++ JS-Deep-Foundation/negtiveZero.js
@@ -0,0 +1,16 @@
+var x = 0
+var y = -0
+
+var a = NaN
+
+console.log(x == y)
+
+console.log(Object.is(x,y))
+
+// console.log(Number.isNaN(a))
+
+// console.log(isNaN('hello world'))
+
+// console.log(Number.isNaN('hello world'))
+
+console.log(NaN !== NaN)
