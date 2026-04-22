319c8ca1efbb6f53f569053a93f19298b7d00725
Ahmed Sheikh <ahmedos099912@gmail.com>
2026-04-22 13:26:50 +0400

    Scope testing

---
+++ JS-Deep-Foundation/scope.js
@@ -0,0 +1,13 @@
+var teacher = 'kyle'
+
+function helloTechers(){
+    teacher = 'omad'
+    topic = 'devops'
+
+    console.log(teacher,topic)
+}
+
+helloTechers()
+
+console.log(teacher)
+console.log(topic)
